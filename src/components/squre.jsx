20f8bfccc83ed1629a11eff2a0869bf156d56caf
natsume0718 <moto718program@gmail.com>
2021-02-01 23:56:15 +0900

    state

--- src/components/squre.jsx
+++ src/components/squre.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 
 export default class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+            <button className="square"
+                    onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
+            </button>
+        );
+    }
 }
